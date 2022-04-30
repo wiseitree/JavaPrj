d3f04e9000cb80e4fa9cfe18be7f3801fbf533a2
crossent <crossent@DESKTOP-FVVB2SG>
2022-04-30 12:51:32 +0900

    kor4 추가

--- src/Program.java
+++ src/Program.java
@@ -1,16 +1,16 @@
 public class Program{
 	public static void main(String[] args) {
 		
-		int kor1, kor2, kor3;
+		int kor1, kor2, kor3, kor4;
 		int total;
 		float avg;
 		
 		kor1 = 50;
 		kor2 = 60;
 		kor3 = 70;
-		
-		total = kor1 + kor2+ kor3;
-		avg = total/3;
+		kor4 = 80;
+		total = kor1 + kor2 + kor3 + kor4;
+		avg = total/4;
 		
 		System.out.printf("total is %d\n", total);
 		System.out.printf("avg is %f\n", avg);
