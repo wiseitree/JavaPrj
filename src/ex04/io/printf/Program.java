ce0f6711751894fcc9abd74b62f3946fba3ebf26
crossent <crossent@DESKTOP-FVVB2SG>
2022-05-02 00:38:37 +0900

    java programimg basic

---
+++ src/ex04/io/printf/Program.java
@@ -0,0 +1,30 @@
+package ex04.io.printf;
+public class Program{
+	public static void main(String[] args) {
+		
+		int kor1, kor2, kor3;
+		int total;
+		float avg;
+		
+		kor1 = 50;
+		kor2 = 60;
+		kor3 = 80;
+		total = kor1 + kor2 + kor3;
+		avg = total/3.0f;
+		avg = (float)(total/3.0);
+		//------------------   성적 출력 부분   ---------------------
+		System.out.println("┌──────────────────────────────┐");
+		System.out.println("│            성적출력                     │");
+		System.out.println("└──────────────────────────────┘");
+		
+		System.out.printf("%2$d %3$d %1$d\n",1,2,3);
+		
+		System.out.printf("국어1 : %3d\n", kor1);
+		System.out.printf("국어2 : %3d\n", kor2);
+		System.out.printf("국어3 : %3d\n", kor3);
+		System.out.printf("총점 : %3d\n", total);
+		System.out.printf("평균 : %6.2f\n", avg);
+		System.out.println(" ──────────────────────────────");
+		System.out.println(avg);
+	}
+}
