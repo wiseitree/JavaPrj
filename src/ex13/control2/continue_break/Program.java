596c169ddf6f2a3a08901bca155eb5bd5b788f87
crossent <crossent@DESKTOP-P9KVCMK>
2022-05-04 22:58:46 +0900

    java basic commit

---
+++ src/ex13/control2/continue_break/Program.java
@@ -0,0 +1,30 @@
+package ex13.control2.continue_break;
+
+import java.util.Scanner;
+
+
+
+public class Program{
+	public static void main(String[] args) {
+		int n = 0;
+		
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("값을 sp로 구분해서 5개 이상 입력하세요:");
+				
+		// 3 Enter 시 하나만 들어감, 34 5 6 234 345 54 45 Enter
+		
+		 while(scan.hasNext()) {
+			n = scan.nextInt();
+			
+			if(n<10)
+				continue;
+			
+			if(n>200)
+				break;
+			
+			System.out.println(n);
+		 }
+		
+	}	
+}
