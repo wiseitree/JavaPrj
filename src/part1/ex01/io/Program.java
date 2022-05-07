8132e3f394497226245f6954c22eb083cf2de1fc
crossent <crossent@DESKTOP-FVVB2SG>
2022-05-07 20:36:50 +0900

    commit

---
+++ src/part1/ex01/io/Program.java
@@ -0,0 +1,11 @@
+package part1.ex01.io;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		System.out.write('3'+1);
+		System.out.flush();
+	}
+
+}
---
+++ src/part1/ex10/control2/for_/example/Program.java
@@ -0,0 +1,27 @@
+package part1.ex10.control2.for_.example;
+
+import java.util.Scanner;
+
+
+
+public class Program{
+	public static void main(String[] args) {
+		int total = 0;
+		
+		/*
+		for(int i=0; i<10; i++)
+			total += 1+i;
+		*/
+		
+		/* 3부터 7 더하는거라면 
+		for(int i=0; i<5; i++)
+			total += 3+i;
+		*/ 
+		
+		//n*(a+1)/2
+		//수학을 알면 빠르게 할 수도 있는데, 모르면 모든 것을 절차로 할 수 밖에 없음.
+		total = 5*(3+7)/2;
+				
+		System.out.printf("total is %d\n", total);
+	}
+}
